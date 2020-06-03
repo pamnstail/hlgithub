6190ed54f253cd2848bf59a2fb5db99784535e90
pamnstail <66362475+pamnstail@users.noreply.github.com>
2020-06-03 20:23:06 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello world");
+	
+	return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 10,b = 5;
+
+	printf("%d", a+b);
+	printf("%d", a*b);
+	printf("%d", a-b);
+	printf("%d", a/b);
+
+	return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 11,b = 5;
+
+	printf("%d" , a%b);
+
+	return 0;
+}
